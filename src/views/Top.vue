adba0731ccdb74e2db55b4b5e9ac687fda163719
koichiro59 <rion9092@gmail.com>
2024-08-11 02:06:37 +0900

    antique-cafe

---
+++ src/views/Top.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>Top Pageです</h1>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name:"Top",
+})
+</script>
+
+<style scoped>
+
+</style>
